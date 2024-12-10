437c9e58bc66686297af25f022e93ef8fb5a356c
SaraSanchez <sara_sanchez82221@elpoli.edu.co>
2024-12-10 12:20:30 -0500

    Implement service for products

---
+++ src/main/java/fashion_line/app/Service/IProductService.java
@@ -0,0 +1,15 @@
+package fashion_line.app.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+import fashion_line.app.Entity.Product;
+
+public interface IProductService {
+    public Product create(Product product);
+    public List<Product> findAll();
+    public Optional<Product> findByName(String name)  throws RuntimeException;
+    public Optional<Product> findByCategory(String category)  throws RuntimeException;
+    public Product update(Product product, Long id)  throws RuntimeException;
+    public void delete(Long id) throws RuntimeException;
+}
