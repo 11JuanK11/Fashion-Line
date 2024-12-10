01b1240784b48e7908890588774ee6849bc73056
JuanK <camiloalzatebedoya15@gmail.com>
2024-12-10 10:46:27 -0500

    First Commit

---
+++ src/main/java/fashion_line/app/AppApplication.java
@@ -0,0 +1,13 @@
+package fashion_line.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApplication.class, args);
+	}
+
+}
